015166f1b117bbb209387f55a6d87f39429de067
Martin Vysny <martin.vysny@ericsson.com>
2018-02-26 15:27:18 +0200

    Added test for create category

--- src/test/kotlin/com/vaadin/starter/beveragebuddy/ui/CategoriesListTest.kt
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/ui/CategoriesListTest.kt
@@ -1,12 +1,13 @@
 package com.vaadin.starter.beveragebuddy.ui
 
-import com.github.karibu.testing.MockVaadin
-import com.github.karibu.testing._get
-import com.github.karibu.testing._size
-import com.github.karibu.testing.autoDiscoverViews
+import com.github.karibu.testing.*
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.dynatest.expectList
+import com.github.vokorm.findAll
 import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.starter.beveragebuddy.Bootstrap
 import com.vaadin.starter.beveragebuddy.backend.Category
 import org.junit.*
@@ -29,5 +30,20 @@ class CategoriesListTest : DynaTest({
 
         val grid = _get<Grid<*>>()
         expect(1) { grid.dataProvider._size() }
+    }
+
+    test("create new category") {
+        UI.getCurrent().navigateTo("categories")
+        _get<Button> { caption = "New category" } ._click()
+
+        // make sure that the "New Category" dialog is opened
+        _get<CategoryEditorDialog>()
+
+        // do the happy flow: fill in the form with valid values and click "Save"
+        _get<TextField> { caption = "Category Name" } .value = "Beer"
+        _get<Button> { caption = "Save" } ._click()
+
+        _find<CategoryEditorDialog> { count = 0..0 }   // expect the dialog to close
+        expectList("Beer") { Category.findAll().map { it.name } }
     }
 })
