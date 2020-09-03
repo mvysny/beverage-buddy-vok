0c2a9a26d63ad6b3cc7a022862fd418d78b965c3
Martin Vysny <mavi@vaadin.com>
2020-09-03 18:06:05 +0300

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,8 +64,8 @@ dependencies {
     implementation("eu.vaadinonkotlin:vok-rest:$vaadinonkotlin_version")
 
     // testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.3")
-    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.16")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.4")
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.17")
     testImplementation("eu.vaadinonkotlin:vok-rest-client:$vaadinonkotlin_version")
     testImplementation("org.eclipse.jetty.websocket:websocket-server:9.4.12.v20180830")
 
