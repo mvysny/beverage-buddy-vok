674aabf5d656ae9d8169b558a5483e51cc0b0fb4
Martin Vysny <mavi@vaadin.com>
2020-11-09 09:21:49 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.30")
 
     // db
-    implementation("org.flywaydb:flyway-core:6.1.4")
+    implementation("org.flywaydb:flyway-core:7.1.1")
     implementation("com.h2database:h2:1.4.200") // remove this and replace it with a database driver of your choice.
 
     // REST
