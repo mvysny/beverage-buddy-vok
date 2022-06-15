644c49122cfebe1893bf19faed4ef6f316324c70
Martin Vysny <mavi@vaadin.com>
2022-06-15 08:27:13 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // The Beverage Buddy sample project ported to Kotlin.
 // Original project: https://github.com/vaadin/beverage-starter-flow
 
-val vaadinonkotlin_version = "0.12.1"
+val vaadinonkotlin_version = "0.13.0"
 val vaadin_version = "23.1.0"
 
 plugins {
@@ -58,7 +58,7 @@ dependencies {
     implementation("eu.vaadinonkotlin:vok-rest:$vaadinonkotlin_version")
 
     // testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.13")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.15")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
     testImplementation("eu.vaadinonkotlin:vok-rest-client:$vaadinonkotlin_version")
     testImplementation("org.eclipse.jetty.websocket:websocket-server:9.4.44.v20210927") // vok-rest uses Javalin 4.3.0 which depends on Jetty 9.x
