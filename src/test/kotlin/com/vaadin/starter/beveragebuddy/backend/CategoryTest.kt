bee792c65591ef6b1046cdde90f1c7e24815206d
Martin Vysny <mavi@vaadin.com>
2023-10-02 09:37:55 +0300

    Test category deletion

--- src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/CategoryTest.kt
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/CategoryTest.kt
@@ -1,14 +1,38 @@
 package com.vaadin.starter.beveragebuddy.backend
 
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.dynatest.expectList
+import com.vaadin.starter.beveragebuddy.ui.usingApp
+import java.time.LocalDate
 import kotlin.test.expect
 
 class CategoryTest : DynaTest({
+    usingApp()
+
     group("validation") {
         test("smoke") {
             expect(false) { Category().isValid }
             expect(false) { Category(name = "  ").isValid }
             expect(true) { Category(name = "F").isValid }
+        }
+    }
+
+    group("delete") {
+        test("smoke") {
+            val cat = Category(name = "Foo")
+            cat.save()
+            cat.delete()
+            expectList() { Category.findAll() }
+        }
+        test("deleting category fixes foreign keys") {
+            val cat = Category(name = "Foo")
+            cat.save()
+            val review = Review(name = "Foo", score = 1, date = LocalDate.now(), category = cat.id!!, count = 1)
+            review.save()
+
+            cat.delete()
+            expectList() { Category.findAll() }
+            expect(null) { Review.single().category }
         }
     }
 })
