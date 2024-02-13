22def4682d8a25faad3ec6e2119c4d9952e39ecb
Martin Vysny <mavi@vaadin.com>
2024-02-13 17:26:22 +0200

    gradle: move deps to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vaadin.gradle.getBooleanProperty
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -7,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.22"
     id("application")
-    id("com.vaadin")
+    alias(libs.plugins.vaadin)
 }
 
 defaultTasks("clean", "build")
@@ -19,7 +20,7 @@ repositories {
 tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
-        // to see the exceptions of failed tests in Travis-CI console.
+        // to see the exception stacktraces of failed tests in the CI console
         exceptionFormat = TestExceptionFormat.FULL
     }
 }
@@ -28,35 +29,32 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     // Vaadin
-    implementation("com.vaadin:vaadin-core:${properties["vaadinVersion"]}") {
-        afterEvaluate {
-            if (vaadin.productionMode.get()) {
-                exclude(module = "vaadin-dev")
-            }
+    implementation(libs.vaadin.core) {
+        // https://github.com/vaadin/flow/issues/18572
+        if (vaadin.productionMode.map { v -> getBooleanProperty("vaadin.productionMode") ?: v }.get()) {
+            exclude(module = "vaadin-dev")
         }
     }
-    implementation("eu.vaadinonkotlin:vok-framework-vokdb:${properties["vokVersion"]}") {
-        exclude(module = "vaadin-core")
-    }
-    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:12.2")
+    implementation(libs.vok.db)
+    implementation(libs.vaadin.boot)
 
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation(libs.hikaricp)
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation(libs.slf4j.simple)
 
     // db
-    implementation("org.flywaydb:flyway-core:9.22.3") // newest version: https://repo1.maven.org/maven2/org/flywaydb/flyway-core/
-    implementation("com.h2database:h2:2.2.224") // remove this and replace it with a database driver of your choice.
+    implementation(libs.flyway) // newest version: https://repo1.maven.org/maven2/org/flywaydb/flyway-core/
+    implementation(libs.h2) // remove this and replace it with a database driver of your choice.
 
     // REST
-    implementation("eu.vaadinonkotlin:vok-rest:${properties["vokVersion"]}")
+    implementation(libs.vok.rest.server)
 
     // testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.2")
-    testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
-    testImplementation("eu.vaadinonkotlin:vok-rest-client:${properties["vokVersion"]}")
+    testImplementation(libs.karibu.testing)
+    testImplementation(libs.dynatest)
+    testImplementation(libs.vok.rest.client)
 }
 
 tasks.withType<KotlinCompile> {
