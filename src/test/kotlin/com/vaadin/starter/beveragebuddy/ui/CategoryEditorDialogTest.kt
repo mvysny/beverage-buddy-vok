e0ca4be9954d7a69f900590cd58e0a76cdba52ce
Martin Vysny <mavi@vaadin.com>
2025-12-05 17:16:30 +0200

    more tests

--- src/test/kotlin/com/vaadin/starter/beveragebuddy/ui/CategoryEditorDialogTest.kt
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/ui/CategoryEditorDialogTest.kt
@@ -13,33 +13,51 @@ import kotlin.test.expect
  * Tests the UI. Uses the Browserless Testing approach as provided by the [Karibu Testing](https://github.com/mvysny/karibu-testing) library.
  */
 class CategoryEditorDialogTest : AbstractAppTest() {
-    @Test fun `create new category`() {
-        CategoryEditorDialog {} .createNew()
+    @Test
+    fun `create new category`() {
+        CategoryEditorDialog {}.createNew()
 
         // make sure that the "New Category" dialog is opened
         _expectOne<EditorDialogFrame<*>>()
 
         // do the happy flow: fill in the form with valid values and click "Save"
-        _get<TextField> { label = "Category Name" } .value = "Beer"
-        _get<Button> { text = "Create" } ._click()
+        _get<TextField> { label = "Category Name" }.value = "Beer"
+        _get<Button> { text = "Create" }._click()
         expectNotifications("Category successfully added.")
 
         _expectNone<EditorDialogFrame<*>>()     // expect the dialog to close
         expect("Beer") { Category.single().name }
     }
 
-    @Test fun `edit existing category`() {
-        val cat = Category(name = "Foo")
-        cat.save()
-
-        CategoryEditorDialog {} .edit(cat)
+    @Test
+    fun `dont create category with blank name`() {
+        CategoryEditorDialog {}.createNew()
 
         // make sure that the "New Category" dialog is opened
         _expectOne<EditorDialogFrame<*>>()
 
         // do the happy flow: fill in the form with valid values and click "Save"
-        _get<TextField> { label = "Category Name" } .value = "Beer"
-        _get<Button> { text = "Save" } ._click()
+        _get<TextField> { label = "Category Name" }.value = ""
+        _get<Button> { text = "Create" }._click()
+
+        _expectOne<EditorDialogFrame<*>>()     // expect the dialog to close
+        _get<TextField> { label = "Category Name" }._expectInvalid()
+        expectList() { Category.findAll() }
+    }
+
+    @Test
+    fun `edit existing category`() {
+        val cat = Category(name = "Foo")
+        cat.save()
+
+        CategoryEditorDialog {}.edit(cat)
+
+        // make sure that the "New Category" dialog is opened
+        _expectOne<EditorDialogFrame<*>>()
+
+        // do the happy flow: fill in the form with valid values and click "Save"
+        _get<TextField> { label = "Category Name" }.value = "Beer"
+        _get<Button> { text = "Save" }._click()
         expectNotifications("Category successfully saved.")
 
         _expectNone<EditorDialogFrame<*>>()     // expect the dialog to close
